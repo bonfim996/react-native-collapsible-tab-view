9de11d6f337d170ea9a1500517e5ed5c2b1274cc
Vitor Lelis <vitorclelis96@gmail.com>
2022-07-08 15:55:05 -0300

    lib folder and package.json updates

---
+++ lib/typescript/src/MaterialTabBar/Indicator.d.ts
@@ -0,0 +1,5 @@
+import React from 'react';
+import { IndicatorProps } from './types';
+declare const IndicatorComponent: JSX.Element;
+declare const Indicator: React.FC<IndicatorProps>;
+export { Indicator, IndicatorComponent };
